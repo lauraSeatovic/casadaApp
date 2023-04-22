bc0a67aa19a64bd5d368417fcfd24289fce32000
lauraSeatovic <laura.seatovic@gmail.com>
2023-04-22 16:45:01 +0200

    Set up a spring boot project for casada api

---
+++ casadaApi/src/main/java/com/casada/casadaApi/CasadaApiApplication.java
@@ -0,0 +1,13 @@
+package com.casada.casadaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasadaApiApplication.class, args);
+	}
+
+}
