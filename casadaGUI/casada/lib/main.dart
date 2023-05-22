c04e459a62da89627f00b93d203b9f1c59fda824
lauraSeatovic <laura.seatovic@gmail.com>
2023-05-22 18:40:05 +0200

    Change app primary color

--- casadaGUI/casada/lib/main.dart
+++ casadaGUI/casada/lib/main.dart
@@ -9,7 +9,26 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final customPrimaryColor = MaterialColor(
+      0xFFB00041,
+  <int, Color>{
+    50: Color(0xFFFCE4EC),
+    100: Color(0xFFF8BBD0),
+    200: Color(0xFFF48FB1),
+    300: Color(0xFFF06292),
+    400: Color(0xFFEC407A),
+    500: Color(0xFFE91E63),
+    600: Color(0xFFD81B60),
+    700: Color(0xFFC2185B),
+    800: Color(0xFFAD1457),
+    900: Color(0xFF880E4F),
+  },
+    );
     return MaterialApp(
+      
+      theme: ThemeData(
+        primarySwatch: customPrimaryColor, // Change the primary color to deep purple
+      ),
       title: "My App",
       home: MyHomePage(),
     );
